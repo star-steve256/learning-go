3737e78b5258b7c7391d38e344d0c4d409695715
Steve Htun Thiha <277367549+star-steve256@users.noreply.github.com>
2026-04-23 23:27:57 +0800

    feat(catfact_api): Add multiple api calls options, concurrency not added yet

--- basic_programs/catfact_api_display/main.go
+++ basic_programs/catfact_api_display/main.go
@@ -8,9 +8,43 @@ import (
 	"net/http"
 )
 
+// DISCLAIMER
+/* There may be a better way to fetch APIs with concurrency,
+   but for now, sequential call and wait will be used. */
+
 func main() {
 	fmt.Println("===== Catfact API display =====")
+	var optionInput int
 
+	for {
+		fmt.Print(`Enter option:
+1. Call API once
+2. Call API multiple times.
+0. Exit
+>> `)
+
+		fmt.Scan(&optionInput)
+
+		if optionInput == 1 {
+			requestApi(1)
+		} else if optionInput == 2 {
+			var numOfApiCalls int
+			fmt.Print("Number of API calls >> ")
+			fmt.Scan(&numOfApiCalls)
+
+			for i := 0; i < numOfApiCalls; i++ {
+				requestApi(i + 1)
+			}
+		} else if optionInput == 0 {
+			break
+		}
+		fmt.Println("===========================")
+	}
+
+	fmt.Println("Program terminated.")
+}
+
+func requestApi(index int) {
 	var responseBody struct {
 		Fact string `json:"fact"`
 	}
@@ -32,6 +66,5 @@ func main() {
 		log.Fatal("ERROR at json decoding:", err)
 	}
 
-	fmt.Println("Fact:", responseBody.Fact)
+	fmt.Printf("#%d Fact: %s\n", index, responseBody.Fact)
 }
-
